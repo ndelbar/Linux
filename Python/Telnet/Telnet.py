3bee3e84f888600390fd1d832d6d63193c9a27f7
Nick Delbar <ndelbar@gmail.com>
2017-01-16 11:27:25 -0600

    Added some custom analyzation for myself. I need to make it more universal

--- Python/Telnet/Telnet.py
+++ Python/Telnet/Telnet.py
@@ -6,17 +6,28 @@ def listening():
 	HOST = "midnightsun2.org"
 	global tn
 	tn = telnetlib.Telnet(HOST)
-	while 1:
+	while bRun:
 		strMessage = tn.read_very_eager().decode('ascii')
 		if (strMessage):
 			print(strMessage)
 		
 
 def userInput():
-	while 1:
+	while bRun:
 		strInput = raw_input()
+		strInput = analyzeInput(strInput)
 		tn.write(strInput.encode('ascii') + b"\n")
 
+
+def analyzeInput(strInput):
+	if strInput == "strich":
+		print ("That's my name!")
+	
+	return strInput	
+
+
+global bRun 
+bRun = True
 tListening = threading.Thread(target = listening)
 tListening.start()
 tUserInput = threading.Thread(target = userInput)
