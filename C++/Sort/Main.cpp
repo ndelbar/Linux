0f3c5d98905d1d2ba8c261e2d87cdc8ab983d4ac
Nicholas Delbar <ndelbar@gmail.com>
2018-09-15 00:14:37 +0000

    Creating sorting branch to practice sorting algorithms

---
+++ C++/Sort/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int myArray[] = {1,5,4,2,3,6,7};
+
+int main()
+{
+	for (int i =0; i < 7; i++)
+	{
+		cout << myArray[i] << endl;
+	}	
+}
